2a6e3de957711d6d689e65ebe077ad8d5c150c03
sagar hussain <160107305+simplegithubr@users.noreply.github.com>
2024-06-27 15:56:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("*****code-with-kinza-My-Bank-console-App*****");
+//Bank Account Class
+class BankAccount {
+    accountNumber;
+    balance;
+    constructor(accountNumber, balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    //Debit Mony
+    withdraw(amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            console.log(`withdrawal of $${amount} successful. remaining balance : $${this.balance}`);
+        }
+        else {
+            console.log("Insufficient balance.");
+        }
+    }
+    // credit mony
+    deposit(amount) {
+        if (amount > 100) {
+            amount -= 1; //
+        }
+        this.balance += amount;
+        console.log(`Deposit of $${amount} successful. Remaining balance $${this.balance}`);
+    }
+    // check balance
+    checkBalance() {
+        console.log(`Current Balance: $${this.balance}`);
+    }
+}
+//coustomer class
+class Customer {
+    firstName;
+    lastName;
+    gender;
+    age;
+    mobileNumber;
+    account;
+    constructor(firstNumber, lastName, gender, age, mobileNumber, account) {
+        this.firstName = firstNumber;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.mobileNumber = mobileNumber;
+        this.account = account;
+    }
+}
+//create bank account
+const accounts = [
+    new BankAccount(1001, 500),
+    new BankAccount(1002, 1000),
+    new BankAccount(1003, 2000)
+];
+//create customers
+const customers = [
+    new Customer("humza", "syed", "male", 35, 3214576523, accounts[0]),
+    new Customer("kinza", "sheikh", "female", 25, 3214769456, accounts[1]),
+    new Customer("haniya", "syed", "female", 34, 3219845654, accounts[2])
+];
+// function to interact with bank account
+async function service() {
+    do {
+        const accountNumberInput = await inquirer.prompt({
+            name: "accountNumber",
+            type: "number",
+            message: "Enter your Account Number"
+        });
+        const customer = customers.find(customer => customer.account.accountNumber === accountNumberInput.accountNumber);
+        if (customer) {
+            console.log(`welcome, ${customer.firstName} ${customer.lastName}!\n`);
+            const ans = await inquirer.prompt([{
+                    name: "select",
+                    type: "list",
+                    message: "select an operation",
+                    choices: ["Desposit", "Withdraw", "Chek Balance", "Exit"]
+                }]);
+            switch (ans.select) {
+                case "Desposit":
+                    const depositAmount = await inquirer.prompt({
+                        name: "amount",
+                        type: "number",
+                        message: "Enter the amount to deposit:"
+                    });
+                    customer.account.deposit(depositAmount.amount);
+                    break;
+                case "Withdraw":
+                    const withdrawAmount = await inquirer.prompt({
+                        name: "amount",
+                        type: "number",
+                        message: "Enter the amount to withdraw:"
+                    });
+                    customer.account.withdraw(withdrawAmount.amount);
+                    break;
+                case "Chek Balance":
+                    customer.account.checkBalance();
+                    break;
+                case "Exit":
+                    console.log("Exiting bank program....");
+                    console.log("\nthank you for using bank services. Have a great day!");
+                    return;
+            }
+        }
+        else {
+            console.log("Invalid account number. please tray again");
+        }
+    } while (true);
+}
+service();
